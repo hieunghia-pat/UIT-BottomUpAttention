fc78157b5f9423891a4a4f6b0e886fabeea1c87a
hieunghia-pat <nghianhox1892001@gmail.com>
2021-08-20 21:08:21 +0700

    make generate_tsv.py run locally

--- tools/generate_tsv.py
+++ tools/generate_tsv.py
@@ -2,29 +2,35 @@
    separate tsv file that can be merged later (e.g. by using merge_tsv function). 
    Modify the load_image_ids script as necessary for your data location. """
 
-# Example:
-# python tools/generate_tsv.py --gpu 0 --cfg experiments/cfgs/faster_rcnn_end2end_resnet.yml --def models/vg/ResNet-101/faster_rcnn_end2end_final/test.prototxt --out uitviic_train_resnet101_faster_rcnn_genome.tsv --net data/faster_rcnn_models/resnet101_faster_rcnn_final.caffemodel --split /mnt/f9e5fe40-5d81-46dc-8450-9c1e67eff197/Projects/object_relation_transformer/data/train.json
+'''
+Example:
+python3 tools/generate_tsv.py \
+        --gpu 0 \
+        --cfg experiments/cfgs/faster_rcnn_end2end_resnet.yml \
+        --def models/vg/ResNet-101/faster_rcnn_end2end_final/test.prototxt \
+        --out uitviic_val_resnet101_faster_rcnn_genome.tsv \
+        --net data/faster_rcnn_models/resnet101_faster_rcnn_final.caffemodel \
+        --split /mnt/f9e5fe40-5d81-46dc-8450-9c1e67eff197/Projects/object_relation_transformer/data/val.json \
+        --base-dir /mnt/f9e5fe40-5d81-46dc-8450-9c1e67eff197/Projects/UIT-ViIC
+'''
 
 import sys
-import _init_paths
-
-sys.path = _init_paths.get_additional_path()
-sys.path = ["/content/UIT-BottomUpAttention/", "/content/UIT-BottomUpAttention/lib"] + sys.path
+import _init_paths # initialize path for faster_rcnn modules
 
 from fast_rcnn.config import cfg, cfg_from_file, cfg_from_list
 from fast_rcnn.test import im_detect,_get_blobs
 from fast_rcnn.nms_wrapper import nms
 from utils.timer import Timer
+from tqdm import tqdm
 
 import caffe
 import argparse
 import pprint
-import time, os, sys
+import os, sys
 import base64
 import numpy as np
 import cv2
 import csv
-from multiprocessing import Process
 import random
 import json
 
@@ -38,32 +44,6 @@ MIN_BOXES = 10
 MAX_BOXES = 100
 
 def load_image_ids(base_dir, split_name):
-    ''' Load a list of (path,image_id tuples). Modify this to suit your data locations. '''
-    '''
-    split = []
-    if split_name == 'coco_test2014':
-      with open('/data/coco/annotations/image_info_test2014.json') as f:
-        data = json.load(f)
-        for item in data['images']:
-          image_id = int(item['id'])
-          filepath = os.path.join('/data/test2014/', item['file_name'])
-          split.append((filepath,image_id))
-    elif split_name == 'coco_test2015':
-      with open('/data/coco/annotations/image_info_test2015.json') as f:
-        data = json.load(f)
-        for item in data['images']:
-          image_id = int(item['id'])
-          filepath = os.path.join('/data/test2015/', item['file_name'])
-          split.append((filepath,image_id))
-    elif split_name == 'genome':
-      with open('/data/visualgenome/image_data.json') as f:
-        for item in json.load(f):
-          image_id = int(item['image_id'])
-          filepath = os.path.join('/data/visualgenome/', item['url'].split('rak248/')[-1])
-          split.append((filepath,image_id))      
-    else:
-      print('Unknown split')
-    '''
     split = []
     with open(split_name, "r") as file:
         data = json.load(file)
@@ -119,7 +99,7 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description='Generate bbox output from a Fast R-CNN network')
     parser.add_argument('--gpu', dest='gpu_id', help='GPU id(s) to use',
-                        default='0', type=str)
+                        default='0', type=int)
     parser.add_argument('--def', dest='prototxt',
                         help='prototxt file defining the network',
                         default=None, type=str)
@@ -148,63 +128,15 @@ def parse_args():
     return args
 
     
-def generate_tsv(gpu_id, prototxt, weights, image_ids, outfile):
-    # First check if file exists, and if it is complete
-    wanted_ids = set([int(image_id[1]) for image_id in image_ids])
-    found_ids = set()
-    if os.path.exists(outfile):
-        with open(outfile) as tsvfile:
-            reader = csv.DictReader(tsvfile, delimiter='\t', fieldnames = FIELDNAMES)
-            for item in reader:
-                found_ids.add(int(item['image_id']))
-    missing = wanted_ids - found_ids
-    if len(missing) == 0:
-        print('GPU {:d}: already completed {:d}'.format(gpu_id, len(image_ids)))
-    else:
-        print('GPU {:d}: missing {:d}/{:d}'.format(gpu_id, len(missing), len(image_ids)))
-    if len(missing) > 0:
-        caffe.set_mode_cpu()
-        caffe.set_device(gpu_id)
-        net = caffe.Net(prototxt, caffe.TEST, weights=weights)
-        with open(outfile, 'ab') as tsvfile:
-            writer = csv.DictWriter(tsvfile, delimiter = '\t', fieldnames = FIELDNAMES)   
-            _t = {'misc' : Timer()}
-            count = 0
-            for im_file,image_id in image_ids:
-                if int(image_id) in missing:
-                    _t['misc'].tic()
-                    writer.writerow(get_detections_from_im(net, im_file, image_id))
-                    _t['misc'].toc()
-                    if (count % 100) == 0:
-                        print('GPU {:d}: {:d}/{:d} {:.3f}s (projected finish: {:.2f} hours)'
-                                .format(gpu_id, count+1, len(missing), _t['misc'].average_time, 
-                                _t['misc'].average_time*(len(missing)-count)/3600))
-                    count += 1
-
-def merge_tsvs():
-    test = ['/work/data/tsv/test2015/resnet101_faster_rcnn_final_test.tsv.%d' % i for i in range(8)]
-
-    outfile = '/work/data/tsv/merged.tsv'
-    with open(outfile, 'ab') as tsvfile:
-        writer = csv.DictWriter(tsvfile, delimiter = '\t', fieldnames = FIELDNAMES)   
-        
-        for infile in test:
-            with open(infile) as tsv_in_file:
-                reader = csv.DictReader(tsv_in_file, delimiter='\t', fieldnames = FIELDNAMES)
-                for item in reader:
-                    try:
-                      writer.writerow(item)
-                    except Exception as e:
-                      print(e)
-
-                      
+def generate_tsv(net, image_ids, outfile):
+  with open(outfile, 'a+') as tsvfile:
+      writer = csv.DictWriter(tsvfile, delimiter = '\t', fieldnames = FIELDNAMES)   
+      for im_file, image_id in tqdm(image_ids):
+        writer.writerow(get_detections_from_im(net, im_file, image_id))
      
 if __name__ == '__main__':
 
     args = parse_args()
-
-    print('Called with args:')
-    print(args)
 
     if args.cfg_file is not None:
         cfg_from_file(args.cfg_file)
@@ -212,8 +144,6 @@ if __name__ == '__main__':
         cfg_from_list(args.set_cfgs)
 
     gpu_id = args.gpu_id
-    gpu_list = gpu_id.split(',')
-    gpus = [int(i) for i in gpu_list]
 
     print('Using config:')
     pprint.pprint(cfg)
@@ -222,19 +152,10 @@ if __name__ == '__main__':
     image_ids = load_image_ids(args.base_dir, args.data_split)
     random.seed(10)
     random.shuffle(image_ids)
-    # Split image ids between gpus
-    image_ids = [image_ids[i::len(gpus)] for i in range(len(gpus))]
     
     caffe.init_log()
-    caffe.log('Using devices %s' % str(gpus))
-    procs = []    
-    
-    for i,gpu_id in enumerate(gpus):
-        outfile = '%s.%d' % (args.outfile, gpu_id)
-        p = Process(target=generate_tsv,
-                    args=(gpu_id, args.prototxt, args.caffemodel, image_ids[i], outfile))
-        p.daemon = True
-        p.start()
-        procs.append(p)
-    for p in procs:
-        p.join()            
+    caffe.set_mode_gpu()
+    caffe.set_device(gpu_id)
+    net = caffe.Net(args.prototxt, caffe.TEST, weights=args.caffemodel)
+
+    generate_tsv(net, image_ids, args.outfile)           
